263ba7ec7d1b1e827b1b12d87abb918209375774
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2016-09-25 21:03:59 +0530

    assignment

---
+++ Assignment7_1/src/Assignment1.java
@@ -0,0 +1,11 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		int sample=123;
+		
+		String sample1=String.valueOf(sample);
+		System.out.println("String value of integer"+sample1);
+	}
+	
+}
